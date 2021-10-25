062b9d5abdf8b20c752011704f259db45e667fe7
boss <505475064@qq.com>
2021-10-25 23:49:48 +0800

    upload

--- the-quibbler/quibbler-api/src/main/java/com/ps/quibbler/config/MyBatisPlusConfig.java
+++ the-quibbler/quibbler-api/src/main/java/com/ps/quibbler/config/MyBatisPlusConfig.java
@@ -1,6 +1,9 @@
 package com.ps.quibbler.config;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,4 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.ps.quibbler.mapper")
 public class MyBatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return interceptor
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
 }
