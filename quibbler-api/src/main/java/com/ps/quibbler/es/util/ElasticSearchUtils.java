52569941334e0121c7ca1e649c80db56c7f0d7d9
paksu <bossfang39@gmail.com>
2022-03-03 23:30:13 +0800

    add es util

--- quibbler-api/src/main/java/com/ps/quibbler/es/util/ElasticSearchUtils.java
+++ quibbler-api/src/main/java/com/ps/quibbler/es/util/ElasticSearchUtils.java
@@ -5,9 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -15,13 +25,23 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -131,7 +151,7 @@ public class ElasticSearchUtils {
             return createDocument(object, index);
         }
         if (this.existsById(index, id)) {
-            return this.updateDocumentByIdNoRealTime(object, index, id);
+            return this.updateDocumentByIdRealTime(object, index, id);
         } else {
             return createDocument(object, index, id);
         }
@@ -146,12 +166,12 @@ public class ElasticSearchUtils {
      * @return
      * @throws IOException
      */
-    public String createDocument(Object object, String index, String id) throws IOException{
+    public String createDocument(Object object, String index, String id) throws IOException {
         if (null == id) {
             return createDocument(object, index);
         }
         if (this.existsById(index, id)) {
-            return this.updateDocumentByIdNoRealTime(object, index, id);
+            return this.updateDocumentByIdRealTime(object, index, id);
         }
         //建立请求
         IndexRequest request = new IndexRequest(index);
@@ -173,7 +193,185 @@ public class ElasticSearchUtils {
      * @return
      * @throws IOException
      */
-    public String createDocument(Object object,String index) throws IOException {
+    public String createDocument(Object object, String index) throws IOException {
         return createDocument(object, index, UUID.randomUUID().toString().replace("-", "").toUpperCase());
+    }
+
+    /**
+     * 通过ID删除文档
+     *
+     * @param index
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    public String deleteDocumentById(String index, String id) throws IOException {
+        DeleteRequest deleteRequest = new DeleteRequest(index, id);
+        DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        return deleteResponse.getId();
+    }
+
+    /**
+     * 通过ID更新文档
+     *
+     * @param object
+     * @param index
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    public String updateDocumentById(Object object, String index, String id) throws IOException {
+        UpdateRequest updateRequest = new UpdateRequest(index, id);
+        updateRequest.timeout("1s");
+        updateRequest.doc(JacksonUtil.toJSONString(object), XContentType.JSON);
+        UpdateResponse updateResponse = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+
+        log.info("Update Document success,index:{},id:{},updateResponseId:{}", index, id, updateResponse.getId());
+        return updateResponse.getId();
+    }
+
+    /**
+     * 通过ID实时更新文档
+     *
+     * @param object
+     * @param index
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    public String updateDocumentByIdRealTime(Object object, String index, String id) throws IOException {
+        //更新请求
+        UpdateRequest updateRequest = new UpdateRequest(index, id);
+
+        //确保文档实时更新
+        updateRequest.setRefreshPolicy("wait_for");
+
+        updateRequest.timeout("1s");
+        updateRequest.doc(JacksonUtil.toJSONString(object), XContentType.JSON);
+        //执行更新请求
+        UpdateResponse updateResponse = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+
+        log.info("Update Document real time success,index:{},id:{},updateResponseId:{}", index, id, updateResponse.getId());
+        return updateResponse.getId();
+    }
+
+    /**
+     * 通过ID获取文档
+     *
+     * @param index
+     * @param id
+     * @param fields
+     * @return
+     * @throws IOException
+     */
+    public Map<String, Object> getDocumentById(String index, String id, String fields) throws IOException {
+        GetRequest request = new GetRequest(index, id);
+        if (StringUtils.isNotEmpty(fields)) {
+            request.fetchSourceContext(new FetchSourceContext(true, fields.split(","), Strings.EMPTY_ARRAY));
+        }
+        GetResponse response = restHighLevelClient.get(request, RequestOptions.DEFAULT);
+        return response.getSource();
+    }
+
+    /**
+     * 通过ID判断文件是否存在
+     *
+     * @param index
+     * @param id
+     * @return
+     * @throws IOException
+     */
+    public boolean existsById(String index, String id) throws IOException {
+        GetRequest request = new GetRequest(index, id);
+        //不获取返回的_source的上下文
+        request.fetchSourceContext(new FetchSourceContext(false));
+        request.storedFields("_none_");
+        return restHighLevelClient.exists(request, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 批量插入文档
+     *
+     * @param index
+     * @param objects
+     * @return false 表示成功
+     */
+    public boolean bulkPost(String index, List<?> objects) {
+        BulkRequest bulkRequest = new BulkRequest();
+        BulkResponse response = null;
+        for (Object object : objects) {
+            IndexRequest request = new IndexRequest(index);
+            request.source(JacksonUtil.toJSONString(object), XContentType.JSON);
+            bulkRequest.add(request);
+        }
+        try {
+            response = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null != response && response.hasFailures();
+    }
+
+    /**
+     * 获取低水平客户端
+     *
+     * @return
+     */
+    public RestClient getLowLevelClient() {
+        return restHighLevelClient.getLowLevelClient();
+    }
+
+    /**
+     * 高亮结果集
+     *
+     * @param searchResponse
+     * @param highlightField
+     * @return
+     */
+    private List<Map<String, Object>> setSearchResponse(SearchResponse searchResponse, String highlightField) {
+
+        // 存放解析结果
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            Map<String, HighlightField> high = hit.getHighlightFields();
+            HighlightField title = high.get(highlightField);
+            // 初始查询结果
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            // 解析高亮字段，并将原来的字段替换为高亮字段
+            if (title != null) {
+                Text[] texts = title.fragments();
+                StringBuilder nTitle = new StringBuilder();
+                for (Text text : texts) {
+                    nTitle.append(text);
+                }
+                sourceAsMap.put(highlightField, nTitle.toString());
+            }
+            list.add(sourceAsMap);
+        }
+        return list;
+    }
+
+    public List<Map<String, Object>> searchListData(String index, SearchSourceBuilder query, String highlightField) throws IOException {
+
+        SearchRequest request = new SearchRequest(index);
+
+        // 高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field(highlightField);
+        // 关闭多个高亮
+        highlightBuilder.requireFieldMatch(false);
+        highlightBuilder.preTags("<span style='color:red'>");
+        highlightBuilder.postTags("</span>");
+        //设置高亮配置
+        query.highlighter(highlightBuilder);
+
+        request.source(query);
+        SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+        log.info("totalHist:" + response.getHits().getTotalHits());
+        if (response.status().getStatus() == 200) {
+            //解析文档
+            return setSearchResponse(response, highlightField);
+        }
+        return null;
     }
 }
